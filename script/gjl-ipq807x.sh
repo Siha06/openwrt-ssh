9d7a58c7192eb5ac09812e04ba4e4f4940394fbf
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-16 23:12:32 +0800

    Update gjl-ipq807x.sh

--- script/gjl-ipq807x.sh
+++ script/gjl-ipq807x.sh
@@ -2,4 +2,4 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/Router/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/WiFi6/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
+mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
